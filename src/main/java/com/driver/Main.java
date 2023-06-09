fd2c6922c70ee453516f64f37e8e5ce0836e2cf4
Vedant Walavalkar <vedant.walavalkar@gmail.com>
2023-06-09 16:16:05 +0530

    Method Overrided

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ class A{
 class B extends A{
     @Override
     public String meth(){
-        super.meth();
+//        super.meth();
         return "Method is overridden in Extendend class B";
     }
 }
