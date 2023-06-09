15c93a2275525be934aa61f0855f29189aa88b28
Vedant Walavalkar <vedant.walavalkar@gmail.com>
2023-06-09 23:35:15 +0530

    Method Overriden 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,18 @@
 package com.driver;
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    @Override
-    public String meth(){
-//        System.out.println(super.meth());
-        return "Method is overridden in Extendend class B";
-    }
-}
+
 public class Main {
+    class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    class B extends A{
+        @Override
+        public String meth(){
+//        System.out.println(super.meth());
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args) {
         B obj = new B();
         obj.meth();
