f3a9399b1dbf6b9465bad8ecdc2c0021d0f42c85
Vedant Walavalkar <vedant.walavalkar@gmail.com>
2023-06-09 23:36:51 +0530

    Method Overriden 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,12 @@
 package com.driver;
 
 public class Main {
-    class A{
+    static class A{
         public String meth(){
             return "Invoking method from class A";
         }
     }
-    class B extends A{
+    static class B extends A{
         @Override
         public String meth(){
 //        System.out.println(super.meth());
